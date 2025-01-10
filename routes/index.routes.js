eaaf68ed1869185df3d627b46629859a951f4bf7
Abdulaziz_c5 <abdulazizbekabdusodiqov@gmail.com>
2025-01-10 12:58:54 +0500

    add orders controller and router

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,11 +4,13 @@ const productRouter = require("./product.routes");
 const cartsRouter = require("./carts.routes");
 const cart_itemsRouter = require("./cart_items.routes");
 const contact_messagesRouter = require("./contact_messages.routes");
+const orderRouter = require("./order.routes");
 
 router.use("/users", userRouter);
 router.use("/products", productRouter);
 router.use("/carts", cartsRouter);
 router.use("/cart_items", cart_itemsRouter);
 router.use("/contact_messages", contact_messagesRouter);
+router.use("/orders", orderRouter);
 
 module.exports = router;
