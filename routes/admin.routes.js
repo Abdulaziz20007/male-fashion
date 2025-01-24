162aaf73efe19ec710cb6a58693346ed431e8ff9
Abdulaziz <92323615+Abdulaziz20007@users.noreply.github.com>
2025-01-24 11:52:42 +0500

    admin.routes.js

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,18 +1,3 @@
-const { getAdmin, getAdminById, addAdmin, updateAdminById, deleteAdminById, loginAdmin, logoutAdmin, refreshAdminToken } = require("../controllers/admin.controller")
-
-const adminRouter = require("express").Router()
-
-adminRouter.get("/logout", logoutAdmin)
-adminRouter.get("/refresh", refreshAdminToken)
-adminRouter.get("/", getAdmin)
-adminRouter.get("/:id", getAdminById)
-adminRouter.post("/", addAdmin)
-adminRouter.post("/login", loginAdmin)
-adminRouter.put("/:id", updateAdminById)
-adminRouter.delete("/:id", deleteAdminById)
-
-module.exports = adminRouter;
-=======
 const express = require("express");
 const router = express.Router();
 const { adminGuard } = require("../middleware/admin.guard");
